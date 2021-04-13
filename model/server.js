dd23638a94fb683b1209b1e09d01f16fd15efdc8
Jesus Marenco <jesusmarencodev@gmail.com>
2021-04-13 07:37:12 -0500

    cors

--- model/server.js
+++ model/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 
 class Server {
@@ -22,6 +23,9 @@ class Server {
   middlewares(){
     //desplegar directorio publico
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    //Cors
+    this.pp.use(cors());
   }
 
   configurationSockets(){
